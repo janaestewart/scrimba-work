f278876f4d671a227a107a21f0d5e204d684aaef
janaestewart <janae.stewart94@gmail.com>
2021-06-29 14:13:59 -0400

    incrementing a variable

--- modThree/index.js
+++ modThree/index.js
@@ -2,8 +2,8 @@
 
 //we need some way to store a count. to store that we need a variable
 
-let count = 0 
-console.log(count)
+// let count = 0 
+// console.log(count)
 
 
 // // 1. Create a variable, myAge, and set its value to your age
@@ -17,9 +17,31 @@ console.log(count)
 
 // 1. Create two variables, myAge and humanDogRatio
 
-    let myAge = 27
-    let humanDogRatio = 7
+//     let myAge = 27
+//     let humanDogRatio = 7
 // 2. Multiply the two together and store the result in myDogAge 
-    let myDogAge = myAge * humanDogRatio
+//     let myDogAge = myAge * humanDogRatio
 // 3. Log myDogAge to the console
-console.log(myDogAge)
+// console.log(myDogAge)
+
+
+// let count = 5
+
+// // count = count + 1
+// //instead do
+
+// count += 1
+
+// console.log(count)
+
+// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50
+console.log(bonusPoints)
+bonusPoints += 50
+console.log(bonusPoints)
+// Decrease it down to 25, and then finally increase it to 70
+bonusPoints -= 25
+console.log(bonusPoints)
+bonusPoints += 70
+console.log(bonusPoints)
+// Console.log the value after each step
