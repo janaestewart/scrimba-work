f32c03e4a8d75bba8141092b2d51564d5ea835b5
janaestewart <janae.stewart94@gmail.com>
2021-06-29 14:04:38 -0400

    module three working with javascript

---
+++ modThree/index.js
@@ -0,0 +1,25 @@
+// document.getElementById('count-el').innerText = 5
+
+//we need some way to store a count. to store that we need a variable
+
+let count = 0 
+console.log(count)
+
+
+// // 1. Create a variable, myAge, and set its value to your age
+
+// let myAge = 27
+
+// // 2. Log the myAge variable to the console
+
+// console.log(myAge)
+
+
+// 1. Create two variables, myAge and humanDogRatio
+
+    let myAge = 27
+    let humanDogRatio = 7
+// 2. Multiply the two together and store the result in myDogAge 
+    let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console
+console.log(myDogAge)
