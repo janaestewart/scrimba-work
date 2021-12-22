862ef5bac2ec7507abb94871b13cbe3428a75842
janaestewart <janae.stewart94@gmail.com>
2021-12-21 23:41:18 -0500

    day 4

---
+++ JavaScriptmas/Day4/index.js
@@ -0,0 +1,28 @@
+// const items = ["Candles", "Decorations", "Chocolate"]
+// const checklist = document.getElementById("checklist")
+// var listItem = document.createElement("LI");
+
+// Task:
+// - For each item in the items array, create a div with a class of "checklist-item", which contains a checkbox input and corresponding label.
+// - Make sure that the shopping list can render a checkbox for all the items, even if new items are added to the items array.
+
+// for(let i = 0; i > items.length; i++){
+//     checklist.appendChild(listItem)
+// }
+
+// Stretch goals:
+// - Add an input which allows the user to add more items.
+// - Add a delete button for the items.
+
+const items = ["Candles", "Decorations", "Chocolate"]
+const checklist = document.getElementById("checklist")
+
+for (let i=0; i<items.length; i++) {
+    checklist.innerHTML += `
+         <div class="checklist-item">
+                <input type="checkbox" id="${items[i]}" value="${items[i]}">
+                <label for="${items[i]}" class="strikethrough">${items[i]}</label>
+            </div>
+    `
+}
+
